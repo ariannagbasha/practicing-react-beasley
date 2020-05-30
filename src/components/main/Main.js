4e1622a22f354cddae355fe2f84c883f57cdbf6b
Arianna Basha <ariannagbasha@gmail.com>
2020-05-30 07:19:17 -0500

    Made SignUp Form

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,6 +1,8 @@
 import React from "react";
 import './Main.css'
 
+import SignUpForm from './components/SignUpForm.js'
+
 function Main(props) {
   return (
     <div className="Main">
@@ -10,11 +12,7 @@ function Main(props) {
         We hope to see <em>you</em> <strong>October 13</strong>!
       </p>
 
-      <form className="SignUpForm">
-        <input name="email" placeholder="Email" type="email" />
-        <input name="guests" placeholder="# of guests" type="number" />
-        <button>RSVP</button>
-      </form>
+      <SignUpForm />
 
       <h1 className="dark">Pre-order now!</h1>
       <p className="dark">The cool kids will all be there</p>
--- src/components/sign-up-form/SignUpForm.js
+++ src/components/sign-up-form/SignUpForm.js
@@ -2,5 +2,11 @@ import React from 'react'
 import './SignUpForm.css'
 
 function SignUpForm(props) {
-
+    <form className="SignUpForm">
+        <input name="email" placeholder="Email" type="email" />
+        <input name="guests" placeholder="# of guests" type="number" />
+        <button>RSVP</button>
+    </form>
 }
+
+export default SignUpForm

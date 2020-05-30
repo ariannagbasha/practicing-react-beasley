38aa5120582c8254691b36d98dc98ec9c17c6562
Arianna Basha <ariannagbasha@gmail.com>
2020-05-30 06:47:58 -0500

    Header

---
+++ src/components/album-title/AlbumTitle.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AlbumTitle(props) {
+    return(
+        <h1 className="AlbumTitle">
+
+        </h1>
+    )
+
+}
+export default AlbumTitle
---
+++ src/components/header/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+import './Header.css'
+
+function Header(props) {
+  return (
+    <div className="Header">
+     <div>
+      <h1 class="AlbumTitle">
+        <button>Reveal Album Title</button>
+        <span>Night of the Salamander</span>
+      </h1>
+
+      <h2>
+        The new release from <strong>Beasley the Bard</strong>
+      </h2>
+
+      <img
+        src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1152887/beasley.png"
+        alt="Beasley the Bard"
+      />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
